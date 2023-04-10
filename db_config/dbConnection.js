81c687d221db8f4b852523e7dd1c9cd2b575f24a
KofikTech <kofiktechgh@gmail.com>
2023-04-10 18:27:16 +0000

    changed an .env variable

--- db_config/dbConnection.js
+++ db_config/dbConnection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 module.exports = {
   dbConn: async (req, res, next) => {
     try {
-      const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+      const connect = await mongoose.connect('mongodb+srv://kofiktechgh:aTM2hoBXnXmvrO7y@cluster0.eqem2es.mongodb.net/aamusted_companion_backend?retryWrites=true&w=majority');
       console.log(
         "Database Connected Succesfully to: ",
         connect.connection.host,
